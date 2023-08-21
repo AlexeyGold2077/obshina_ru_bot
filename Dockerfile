c92d515ef814e4c53d5099dfca95fb83c6be1bd5
Alexey Zolotarev <alexgold2077@yandex.ru>
2023-08-21 07:39:34 +0300

    Added logging, Added pip update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python
 WORKDIR /app
 COPY . .
+RUN pip install --upgrade pip
 RUN pip install telebot
 #EXPOSE port
 CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -4,14 +4,17 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_command_responce(message):
+    print(message.text)
     bot.send_message(message.chat.id, f'Здравствуй, {message.from_user.first_name} {message.from_user.last_name} приветствуем тебя в боте русской общины!\nНапиши /help для отображения доступных команд или нажми на кнопку команды ниже.')
 
 @bot.message_handler(commands=['help'])
 def help_command_responce(message):
+    print(message.text)
     bot.send_message(message.chat.id, 'Список доступных комманд:\n/help - список доступных комманд\n/channel - получить ссылку на наш официальный телеграм канал')
 
 @bot.message_handler(commands=['channel'])
 def channel_command_responce(message):
+    print(message.text)
     bot.send_message(message.chat.id, 'Наш официальный канал в телеграм - @obshina_ru')
 
 bot.polling(none_stop=True)
